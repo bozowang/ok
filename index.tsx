a9462bf22fc2a38aa3658eb62dcc1f7cd8de070a
bozowang <chefbozo@gmail.com>
2025-09-26 09:46:47 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
-﻿
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -9,8 +8,4 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
